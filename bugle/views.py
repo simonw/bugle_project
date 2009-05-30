a42f7fc72a2aec8666621c4eb00e639d235cc4fa
Simon Willison <simon@simonwillison.net>
2009-05-30 17:51:35 +0100

    You can now post messages

--- bugle/views.py
+++ bugle/views.py
@@ -1,7 +1,20 @@
 from bugle.shortcuts import render, redirect
+from models import Blast
 
 def homepage(request):
     if request.user.is_anonymous():
-        return redirect('/account/register/')
+        return redirect('/login/')
     
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html', {
+        'blasts': Blast.objects.all().order_by('-created')[:30],
+    })
+
+def post(request):
+    if request.user.is_anonymous():
+        return redirect('/login/')
+    Blast.objects.create(
+        user = request.user,
+        message = request.POST.get('blast', '')
+    )
+    return redirect('/')
+
--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'bugle.views.homepage'),
+    (r'^post/$', 'bugle.views.post'),
     (r'^account/', include('registration.urls')),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
     url(r'^login/$', 'django.contrib.auth.views.login', name="login"),

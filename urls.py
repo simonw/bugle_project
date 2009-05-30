83b058a1d7508da3d7cf93fcda72fd5069d6bb81
Simon Willison <simon@simonwillison.net>
2009-05-30 18:56:06 +0100

    Configured /static/ serving, added jQuery

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -11,7 +12,10 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', name="login"),
     
     (r'^admin/', include(admin.site.urls)),
-
+    
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': os.path.join(settings.OUR_ROOT, 'static')
+    }),
+    
     (r'^([a-zA-Z0-9]+)/$', 'bugle.views.profile'),
-
 )

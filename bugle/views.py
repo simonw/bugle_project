d227946e60d40bcb80c04847820ff80ba8f677f8
Simon Willison <simon@simonwillison.net>
2009-05-30 19:25:54 +0100

    Added Ajax autorefresh

--- bugle/views.py
+++ bugle/views.py
@@ -1,13 +1,17 @@
 from bugle.shortcuts import render, redirect, get_object_or_404
 from models import Blast
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.utils import dateformat
+import simplejson
 
-def homepage(request):
+def homepage(request, autorefresh=False):
     if request.user.is_anonymous():
         return redirect('/login/')
     
     return render(request, 'homepage.html', {
         'blasts': Blast.objects.all().order_by('-created')[:30],
+        'autorefresh': autorefresh,
     })
 
 def post(request):
@@ -26,3 +30,16 @@ def profile(request, username):
     return render(request, 'profile.html', {
         'profile': user,
     })
+
+def since(request):
+    id = request.GET.get('id', 0)
+    blasts = Blast.objects.filter(id__gt = id).order_by('-created')
+    return HttpResponse(simplejson.dumps([{
+        'user': str(b.user),
+        'message': b.message,
+        'created': str(b.created),
+        'date': dateformat.format(b.created, 'jS F'),
+        'time': dateformat.format(b.created, 'H:i'),
+        'colour': '#' + b.colour(),
+        'id': b.id,
+    } for b in blasts]), content_type = 'text/plain')
--- urls.py
+++ urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
+import os
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'bugle.views.homepage'),
+    (r'^autorefresh/$', 'bugle.views.homepage', {'autorefresh': True}),
+    (r'^autorefresh/since/$', 'bugle.views.since'),
     (r'^post/$', 'bugle.views.post'),
     (r'^account/', include('registration.urls')),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),

cb1788cd4948f4b2843bbd63d1cdf9457189f796
Simon Willison <simon@simonwillison.net>
2009-05-31 12:49:22 +0100

    urlize blasts

--- bugle/templatetags/bugle.py
+++ bugle/templatetags/bugle.py
@@ -9,6 +9,7 @@ username_re = re.compile('@[0-9a-zA-Z]+')
 
 @register.filter
 def buglise(s):
+    s = unicode(s)
     def replace_username(match):
         username = match.group(0)[1:]
         try:
@@ -16,4 +17,5 @@ def buglise(s):
         except User.DoesNotExist:
             return '@' + username
         return '<a href="/%s/">@%s</a>' % (username, username)
+    
     return mark_safe(username_re.sub(replace_username, s))

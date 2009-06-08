b3365fb213d6a1dd0ba137b109d51e4cf1a34ae1
Simon Willison <simon@simonwillison.net>
2009-06-08 08:47:12 +0100

    DebugFooter for spotting rogue SQL, currently disabled

--- settings.py
+++ settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#    'debug_middleware.DebugFooter',
 )
 
 ROOT_URLCONF = 'bugle_project.urls'

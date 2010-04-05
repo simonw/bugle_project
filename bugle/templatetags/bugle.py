420694c8546156f1658e11b424c675940cef7c1c
Simon Willison <simon@simonwillison.net>
2010-04-05 18:43:45 +0100

    Optimised buglise function a bit, provided the install doesn't have too many registered users - should really cache this query

--- bugle/templatetags/bugle.py
+++ bugle/templatetags/bugle.py
@@ -10,14 +10,17 @@ username_re = re.compile('@[0-9a-zA-Z]+')
 @register.filter
 def buglise(s):
     s = unicode(s)
+    if not username_re.match(s):
+        return mark_safe(s)
+    
+    usernames = set(User.objects.values_list('username', flat=True))
     def replace_username(match):
         username = match.group(0)[1:]
-        try:
-            u = User.objects.get(username = username)
-        except User.DoesNotExist:
-            if username.lower() == 'all':
-                return '<strong>@all</strong>'
+        if username.lower() == 'all':
+            return '<strong>@all</strong>'
+        if username in usernames:
+            return '<a href="/%s/">@%s</a>' % (username, username)
+        else:
             return '@' + username
-        return '<a href="/%s/">@%s</a>' % (username, username)
     
     return mark_safe(username_re.sub(replace_username, s))

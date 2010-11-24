c6837af1af2939965976bfb45099bf7c2407a9da
George Brocklehurst <george.brocklehurst@gmail.com>
2010-11-24 00:23:23 +0000

    Add more HTTP headers to GhettoOauth
    The official iPhone Twitter client uses 
    HTTP_X_VERIFY_CREDENTIALS_AUTHORIZATION when it's
    connecting to image upload services.

--- twitter_api/middleware/ghetto_oauth.py
+++ twitter_api/middleware/ghetto_oauth.py
@@ -3,14 +3,19 @@ import re
 
 class GhettoOAuthMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
-        user_id = None
-        if 'HTTP_AUTHORIZATION' in request.META and request.META['HTTP_AUTHORIZATION'].startswith('OAuth'):
-            m = re.search(r'oauth_token="(\d+)"', 
-                    request.META['HTTP_AUTHORIZATION'])
-            if m:
-                user_id = m.group(1)
+        user_id = self._get_token_from_header(request, 'HTTP_AUTHORIZATION')
+        if not user_id:
+            user_id = self._get_token_from_header(request, 'HTTP_X_VERIFY_CREDENTIALS_AUTHORIZATION')
+
         if 'oauth_token' in request.GET:
             user_id = request.GET['oauth_token']
         if user_id:
             request.user = User.objects.get(pk=user_id)
         return view_func(request, *view_args, **view_kwargs)
+    
+    def _get_token_from_header(self, request, header):
+        if header in request.META and request.META[header].startswith('OAuth'):
+            m = re.search(r'oauth_token="(\d+)"', request.META[header])
+            if m:
+                return m.group(1)
+

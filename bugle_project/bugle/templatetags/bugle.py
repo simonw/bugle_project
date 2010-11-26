870fc1fc3cf240798796a5ed0a280bf300d19d5f
Ben Firshman <ben@firshman.co.uk>
2010-11-26 01:10:52 +0000

    @user messages will create a link even if the user doesn't exist to save on a query

--- bugle_project/bugle/templatetags/bugle.py
+++ bugle_project/bugle/templatetags/bugle.py
@@ -13,15 +13,12 @@ hashtag_re = re.compile('(^|\s)(#\S+)')
 def buglise(s):
     s = unicode(s)
     
-    usernames = set(User.objects.values_list('username', flat=True))
     def replace_username(match):
         username = match.group(0)[1:]
         if username.lower() == 'all':
             return '<strong>@all</strong>'
-        if username in usernames:
-            return '<a href="/%s/">@%s</a>' % (username, username)
         else:
-            return '@' + username
+            return '<a href="/%s/">@%s</a>' % (username, username)
     
     s = username_re.sub(replace_username, s)
 

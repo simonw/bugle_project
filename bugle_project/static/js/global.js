663917a7b1ca4a32f23335e9b767a33ac633eb5c
Steve Marshall <steve@nascentguruism.com>
2010-11-26 00:00:18 +0000

    Add debugging.

--- bugle_project/static/js/global.js
+++ bugle_project/static/js/global.js
@@ -1,9 +1,10 @@
 jQuery(function($) {
+    console.log( 'bugle 1' );
     if (!$('form#blast').length) {
         return;
     }
+    console.log( 'bugle 2' );
     $('form#blast input:first')[0].focus();
-    $('#extended,#attach').hide();
     var include_code = $('<a href="#" id="include-code">Include some code</a>');
     include_code.toggle(function() {
         $('#extended').show();
@@ -15,6 +16,8 @@ jQuery(function($) {
         $(this).removeClass( 'active' );
         return false;
     });
+    
+    console.log( 'bugle 3' );
     var attach_file = $('<a href="#" class="attach-file">Attach a file</a>');
     attach_file.toggle(function() {
         $('#attach').show();
@@ -25,11 +28,15 @@ jQuery(function($) {
         $(this).removeClass( 'active' );
         return false;
     });
+    
+    console.log( 'bugle 4' );
     var extended_blasts = $( '<span class="extended"></span>' );
     extended_blasts.append(include_code);
     extended_blasts.append(attach_file);
     $( '#blast-base' ).append( extended_blasts );
+    $('#extended,#attach').hide();
 
+    console.log( 'bugle 5' );
     $('#blasts .bundle > div').css('cursor', 'pointer').click(function () {
         $(this).parent().children('ol').toggle();
     });
